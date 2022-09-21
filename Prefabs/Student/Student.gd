d7f023d3af9a4d9fb978edb6a0cbcc6bec4a767a
gamedevnot <89422421+gamedevnot@users.noreply.github.com>
2022-09-21 22:52:50 +0200

    People :)

--- Prefabs/Student/Student.gd
+++ Prefabs/Student/Student.gd
@@ -1,8 +1,13 @@
 extends StaticBody2D
 
 onready var timer = $"%Timer"
+onready var person = $"%Person"
 
 onready var TimeEffect = preload("res://Effects/TimeEffect.tscn")
+
+func _ready():
+	randomize()
+	person.frame = randi() % 7
 
 func start_timer(time):
 	var timeEffect = TimeEffect.instance()
