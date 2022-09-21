4ae6d760ab4c67b5efb772d66898a9cc6c877c38
gamedevnot <89422421+gamedevnot@users.noreply.github.com>
2022-09-21 23:23:54 +0200

    Students :3

--- Prefabs/Student/Student.gd
+++ Prefabs/Student/Student.gd
@@ -1,15 +1,25 @@
 extends StaticBody2D
 
-onready var timer = $"%Timer"
 onready var person = $"%Person"
 
 onready var TimeEffect = preload("res://Effects/TimeEffect.tscn")
 
+var is_on_timer = false
+
 func _ready():
 	randomize()
-	person.frame = randi() % 7
+	person.frame = randi() % 6
 
 func start_timer(time):
+	is_on_timer = true
 	var timeEffect = TimeEffect.instance()
 	add_child(timeEffect)
-	#timer.start(time)
+	timeEffect.animate_timer(time)
+	timeEffect.connect("timer_finished", self, "timer_finished")
+
+func is_on_timer():
+	print(is_on_timer)
+	return is_on_timer
+
+func timer_finished():
+	is_on_timer = false
--- Scenes/GameController.gd
+++ Scenes/GameController.gd
@@ -3,17 +3,24 @@ extends Node2D
 onready var students = $"%Students"
 onready var timer = $"%Timer"
 
+var rate_time = 5
+var active_time = 6.0
+
 func _ready():
 	randomize()
-	timer.start(float(randi() % 5))
+	timer.start(float(randi() % rate_time))
 	timer.connect("timeout", self, "on_timeout")
 
 
 func on_timeout():
 	var children = students.get_children()
 	if children.size() > 0:
-		var child = children[randi() % children.size()]
-		child.start_timer(5)
+		while true:
+			print("checking")
+			var child = children[randi() % children.size()]
+			if !child.is_on_timer:
+				child.start_timer(active_time)
+				break
 
 
 
