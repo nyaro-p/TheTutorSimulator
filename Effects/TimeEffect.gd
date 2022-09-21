afff383df75aabc32844ceeab1e86564c886729b
Jane-Loreen Krey <krey@uni-bremen.de>
2022-09-21 22:15:43 +0200

    Update_1
    >Sprites replaced with better quality
    >Animation for values added/started (?)

--- Effects/TimeEffect.gd
+++ Effects/TimeEffect.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
 onready var mesh = $MeshInstance2D2
-var time = 4.0
+var time = 2.0
 
-func _physics_process(delta):
-	mesh.scale.x = lerp(mesh.scale.x, 1, 2 * delta)
-	mesh.scale.y = mesh.scale.x
+func _ready():
+	var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(mesh, "scale", Vector2(1.0, 1.0), time)
+	tween.connect("finished", self, "finished")
 	
-	if mesh.scale.y > 0.99:
-		get_parent().queue_free()
+func finished():
+	print("fin")
+
+#func _physics_process(delta):
+#	mesh.scale.x = lerp(mesh.scale.x, 1, 2 * delta)
+#	mesh.scale.y = mesh.scale.x
+#
+#	if mesh.scale.y > 0.99:
+#		get_parent().queue_free()
 	
