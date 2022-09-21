91a3b48498eaba7392c59d114fdddfc6a597946f
gamedevnot <89422421+gamedevnot@users.noreply.github.com>
2022-09-21 23:53:44 +0200

    cool animations

--- Effects/TimeEffect.gd
+++ Effects/TimeEffect.gd
@@ -7,9 +7,10 @@ signal timer_finished
 
 func animate_timer(time):
 	var tween := create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
-	tween.tween_property(bounds_mesh, "scale", Vector2(1.0, 1.0), 1.0)
+	tween.tween_property(bounds_mesh, "scale", Vector2(0.2, 0.2), 1.0)
 	tween.parallel().tween_property(growing_mesh, "scale", Vector2(1.0, 1.0), time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
-	tween.parallel().tween_property(growing_mesh, "modulate", Color(1, 0, 0), time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.parallel().tween_property(growing_mesh, "modulate", Color(1, 0, 0, 0.470588), time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	tween.parallel().tween_property(bounds_mesh, "modulate", Color(1, 0, 0, 0.470588), time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 	tween.connect("finished", self, "finished")
 	
 func finished():
