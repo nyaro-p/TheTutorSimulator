fafba6289392046b2a554fe75d2258cf858546df
gamedevnot <89422421+gamedevnot@users.noreply.github.com>
2022-09-23 00:27:50 +0200

    have some time :)

--- Prefabs/UI/Clock.gd
+++ Prefabs/UI/Clock.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 onready var arrow = $ArrowPivot
+onready var tut_time = 20.0
 
 func _ready():
-	var tween := create_tween().set_trans(Tween.TRANS_LINEAR)
-	tween.tween_property(arrow, "rotation_degrees", 360.0, 20.0)
-	tween.connect("finished", self, "time_finished")
+	time_finished()
 
 func time_finished():
-	print("global time finish")
+	var new_rotation = arrow.rotation_degrees + 6.0
+	if new_rotation > 360.0:
+		print("global time finished")
+	else:
+		var time = tut_time / 60
+		var tween := create_tween().set_trans(Tween.TRANS_ELASTIC)
+		tween.tween_property(arrow, "rotation_degrees", new_rotation, time)
+		tween.connect("finished", self, "time_finished")
