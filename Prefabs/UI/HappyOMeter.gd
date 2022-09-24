d68276f512bcda8c72c3dd3b69e3240691916c84
gamedevnot <89422421+gamedevnot@users.noreply.github.com>
2022-09-24 16:15:50 +0200

    small fix

--- Prefabs/UI/HappyOMeter.gd
+++ Prefabs/UI/HappyOMeter.gd
@@ -8,13 +8,19 @@ func _ready() -> void:
 	button.position.y = 61
 
 func increase() -> void:
-	button.position.y -= 6
-	button.position.y = clamp(button.position.y, 35, 83)
-	happiness = 1.0 -((button.position.y - 35) / (83- 35))
-	print(happiness)
+	animate(-1.0)
 
 func decrease() -> void:
-	button.position.y += 6
-	button.position.y = clamp(button.position.y, 35, 83)
-	happiness = 1.0 -((button.position.y - 35) / (83- 35))
-	print(happiness)
+	animate(1.0)
+
+func animate(value : float) -> void:
+	var new_position = button.position + (Vector2(0.0, 5.0) * value)
+	if new_position.y < 35:
+		new_position.y = 35
+	
+	if new_position.y > 87:
+		new_position.y = 87
+	
+	happiness = 1.0 -((new_position.y - 35) / (87- 35))
+	var tween := create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(button, "position", new_position, 0.6)
