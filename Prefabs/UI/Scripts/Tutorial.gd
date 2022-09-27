a79bbdf553f6f0f6683e1b22072c337c6c8875fb
gamedevnot <89422421+gamedevnot@users.noreply.github.com>
2022-09-28 00:09:00 +0200

    Improved Tutorial

--- Prefabs/UI/Scripts/Tutorial.gd
+++ Prefabs/UI/Scripts/Tutorial.gd
@@ -3,18 +3,21 @@ extends Control
 var step := 0
 
 func _ready() -> void:
-	$First.modulate = Color(1, 1, 1, 0)
-	$Second.modulate = Color(1, 1, 1, 0)
+	var fadeIn = GlobalStats.FadeIn.instance()
+	add_child(fadeIn)
+	
+	$New/First.modulate = Color(1, 1, 1, 0)
+	$New/Second.modulate = Color(1, 1, 1, 0)
 
 	var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property($First, "modulate", Color(1, 1, 1, 1), 1.0)
+	tween.tween_property($New/First, "modulate", Color(1, 1, 1, 1), 1.0)
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("interact"):
+	if Input.is_action_pressed("ui_select"):
 		match step:
 			0:
 				var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
-				tween.tween_property($Second, "modulate", Color(1, 1, 1, 1), 1.0)
+				tween.tween_property($New/Second, "modulate", Color(1, 1, 1, 1), 1.0)
 				step += 1
 			1:
 				var fadeOut = GlobalStats.FadeOut.instance()
